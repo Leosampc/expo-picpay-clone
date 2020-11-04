7442d57386597bc026230d624890d9d0f9be170b
Leonardo Cruz <leonardosamp11@gmail.com>
2020-11-04 17:32:28 -0300

    added StatusBar component config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import { StatusBar } from 'react-native';
 import Navigation from '#/Navigation';
 
-const App = () => <Navigation />;
+const App = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#000" />
+    <Navigation />
+  </>
+);
 
 export default App;
