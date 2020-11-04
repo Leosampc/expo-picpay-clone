43774791a9b807d064bf43cf1b76e1f28bc771c1
Leonardo Cruz <leonardosamp11@gmail.com>
2020-11-04 20:39:48 -0300

    added Suggestions and Activities component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import PayButton from './PayButton';
+import Suggestions from './Suggestions';
+import Activities from './Activities';
 import Icon from './Icon';
 
-export { PayButton, Icon };
+export { PayButton, Suggestions, Activities, Icon };
