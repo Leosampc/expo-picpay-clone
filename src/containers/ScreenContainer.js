8b7fc85e33bdaf4fb37444ee2aea5af41c05a0f1
Leonardo Cruz <leonardosamp11@gmail.com>
2020-11-04 20:38:22 -0300

    uses ScrollView and added header props

--- src/containers/ScreenContainer.js
+++ src/containers/ScreenContainer.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import { Layout, Colors } from '#/constants';
 
 const Wrapper = styled.SafeAreaView`
-  background-color: #000;
+  background-color: ${Colors.blackColor};
   flex: 1;
 `;
 
-export default ({ children, ...props }) => (
-  <Wrapper {...props}>{children}</Wrapper>
+const ScrollView = styled.ScrollView``;
+
+const Header = styled.View`
+  height: ${Layout.normalize(50)}px;
+  width: ${Layout.windowWidth}px;
+  padding: 0 ${Layout.normalize(16)}px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export default ({ children, headerComponent, float, ...props }) => (
+  <Wrapper>
+    <ScrollView {...props}>
+      <Header
+        style={{ position: float ? 'absolute' : 'relative', zIndex: 99999 }}
+      >
+        {headerComponent()}
+      </Header>
+      {children}
+    </ScrollView>
+  </Wrapper>
 );
