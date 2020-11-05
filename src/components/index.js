e63af1146d4c917e2fef02b0eb2da02bc47477b6
Leonardo Cruz <leonardosamp11@gmail.com>
2020-11-05 00:28:54 -0300

    added ActivityCard

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable import/no-cycle */
 import PayButton from './PayButton';
 import Suggestions from './Suggestions';
 import Activities from './Activities';
+import ActivityCard from './ActivityCard';
 import Icon from './Icon';
 
-export { PayButton, Suggestions, Activities, Icon };
+export { PayButton, Suggestions, Activities, ActivityCard, Icon };
